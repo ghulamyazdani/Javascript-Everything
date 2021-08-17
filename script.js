5d41b3f8e8c0c70c57873c727c66827c3c6cbf03
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2021-08-17 18:12:28 +0530

    added func

--- script.js
+++ script.js
@@ -134,3 +134,27 @@ function freezeObj(){
     object.freeze(Math_Constant);
 
 }
+// Arrow function
+var myConcat = (arr1, arr2) => arr1.concat(arr2);
+console.log(myConcat([1,2],[3,4,5]))
+
+const realNumberArray=[4,,5.6,-9.8,3.14,42,6,8.34,-2]
+
+const squareList = (arr) => {
+
+const squaredIntegers =  arr.filter(num => Number.isInteger(num)&&num>0).map(x => x*x);
+return squaredIntegers;
+
+};
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
+
+// dosr function
+const sum = sfunction() 
+return function sum(x,y,z){
+const arguments = arguments.
+}
+function add(a,b){
+    return a + b;
+}
+console.log(add(2,4))
