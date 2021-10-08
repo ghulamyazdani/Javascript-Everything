3e0813bc641f159b269184f1137d3f4cd431e2aa
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2021-10-09 00:48:32 +0530

    fem

---
+++ Fem/fem.js
@@ -0,0 +1,12 @@
+var myName = "Ghulam Yazdani";
+var twitterHandle = "iamyazreza";
+var age = 20;
+
+function whoAmI(myName,twitterHandle,age) {
+    console.log(
+        `Hello my name is ${myName} Twitter handle - ${twitterHandle} age- ${age}`
+    );
+    
+}
+
+whoAmI(myName,twitterHandle,age);
