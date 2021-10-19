e258a7c479d8578a4fd9988a0f6fb3a04b173967
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2021-10-19 21:39:53 +0530

    exercises

---
+++ Fem/exercises/primer/ex.js
@@ -0,0 +1,20 @@
+// TODO: define addFavoriteBook(..) function
+function addFavoriteBook(bookName) {
+    if (!bookName.includes('great')) {
+        favoriteBooks.push(bookName);
+        
+    }
+}
+
+// TODO: define printFavoriteBooks() function
+
+
+var favoriteBooks = [];
+
+addFavoriteBook("A Song of Ice and Fire");
+addFavoriteBook("The Great Gatsby");
+addFavoriteBook("Crime & Punishment");
+addFavoriteBook("Great Expectations");
+addFavoriteBook("You Don't Know JS");
+
+// TODO: print out favorite books
