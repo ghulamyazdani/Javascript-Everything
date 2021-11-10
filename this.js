7d99852359a1f9d6cb11948faf08d8408c30ba2e
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2021-11-11 01:25:26 +0530

    prac

--- this.js
+++ this.js
@@ -4,10 +4,33 @@ function Node(data) {
     this.left = null;
     this.right = null;
 }   
-let teacher = "Kyle";
-function teacherChange() {
-    let teacher = "Joy";
-    console.log(teacher);
+// let teacher = "Kyle";
+// function teacherChange() {
+//     let teacher = "Joy";
+//     console.log(teacher);
+// }
+// teacherChange();
+// console.log(teacher);
+
+function ask(question){
+    console.log(this.teacher,question);
 }
-teacherChange();
-console.log(teacher);
+function otherClass(){
+    var myContext = {
+        teacher: ["Joy","Kyle","Karen"]
+    };
+    ask.call(myContext,"What is your name?");
+}
+otherClass();
+
+
+class Workshop{
+    constructor(teacher){
+        this.teacher = teacher;
+    }
+    ask(question){
+        console.log(this.teacher,question);
+    }
+}   
+var deepJS = new Workshop("Kyle");
+deepJS.ask("What is your name?");
