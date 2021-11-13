44b0e90ad7de006435b94fdb5d8767da69f8880a
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2021-11-13 18:12:46 +0530

    prac

--- GameChar.js
+++ GameChar.js
@@ -30,4 +30,29 @@ const game2 = {
     for (const {name, color} of game.suspects) {
         console.log(name, color);
     }
+};
+let nam = [];
+function names(game){
+    for (var key of game.suspects){
+        nam.push(key.name)
+    }
+} 
+
+function createSuspectObject(name){
+    return {
+        name: name,
+        color: name.split(' ')[1],
+        speak() {
+            console.log(`My name is ${name}`);
+        }
+    }
+};
+var suspects = ['Miss scarlet', 'Colonel Mustard', 'Mrs. White', 'Mr. Green'];
+var suspectsList = [];
+for (var i = 0; i < suspects.length; i++) {
+    suspectsList.push(createSuspectObject(suspects[i]));
 }
+
+_.each(suspects, function(name){
+    suspectsList.push(createSuspectObject(name));
+})
