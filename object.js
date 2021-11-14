f0576c07f8a58c8442d5d8fa1197173e7dbc2bf6
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2021-11-15 01:20:02 +0530

    prac

--- object.js
+++ object.js
@@ -2,4 +2,17 @@
 function toString(obj) {
     return Object.prototype.toString.call(obj);
 }
+var increment = function (n) {
+    return n + 1;
+};  
+var square = function (n) {
+    return n * n;
+};
+var doMathSoIDontHaveTo = function (n, func) {
+    return func(n);
+};
+doMathSoIDontHaveTo(5,square);
 
+var increment = (n) => n+1;
+var square = (n) => n*n;
+var doMathSoIDontHaveTo = (n, func) => func(n);
