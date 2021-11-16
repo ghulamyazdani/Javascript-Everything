0c1307228114441dfc9c433e40893b4276cb00c7
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2021-11-17 02:16:31 +0530

    countdown

---
+++ 10projin24hrs/countdown-timer/script.js
@@ -0,0 +1,22 @@
+const daysEm = document.getElementById('days');
+const hoursEm  = document.getElementById('hours');
+const minutesEm = document.getElementById('minutes');
+const secondEm = document.getElementById('second');
+var newYear = "1 jan 2022";
+
+function countDates() {
+var newYearDate = new Date(newYear);
+var currentDate = new Date();
+const days = Math.floor((newYearDate - currentDate) / (1000*60 * 60 * 24));
+const hours = Math.floor((newYearDate - currentDate) / (1000 * 60 * 60)%24);
+const minutes = Math.floor((newYearDate - currentDate) / (1000 * 60)%60);
+const seconds = Math.floor((newYearDate - currentDate) / 1000%60);
+daysEm.innerHTML = days;
+hoursEm.innerHTML = hours;
+minutes.innerHTML = timeFormat(minutes);
+secondEm.innerHTML = timeFormat(seconds);
+console.log(days, hours, minutes,seconds);
+}
+const timeFormat = (time) => {return time<10 ?`0${time}`: time;};
+countDates();
+setInterval(countDates, 1000);
