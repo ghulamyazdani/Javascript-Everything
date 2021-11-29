dad044b0fb6af25c1f4b036f2d9e1d2b615934bc
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2021-11-29 18:43:54 +0530

    commit

--- 10projin24hrs/Todo app/script.js
+++ 10projin24hrs/Todo app/script.js
@@ -1,48 +1,53 @@
-const text = document.getElementById('textarea');
-const lists = document.getElementById('lists');
-const addBtn = document.getElementById('add');
+const text = document.getElementById("textarea");
+const lists = document.getElementById("lists");
+const addBtn = document.getElementById("add");
 const todos = JSON.parse(localStorage.getItem("todos"));
 
 if (todos) {
-    todos.forEach((todo) => {
-        addList(todo);
-    });
+  todos.forEach((todo) => {
+    addList(todo);
+  });
 }
-addBtn.addEventListener('click', function(){
-    if(text.value === ''){
-        alert('Please enter a value');
-    } else {
+addBtn.addEventListener("click", function () {
+  if (text.value === "") {
+    alert("Please enter a value");
+  } else {
     addList(text.value);
     updateLS();
-    }
+  }
 });
-function addList(value) {
-    const list = document.createElement('li');
+function addList(todo) {
+  const list = document.createElement("li");
 
-    list.innerHTML = `
-        <span>${value}</span
+  list.innerHTML = `
+        <span class="val">${todo}</span
         ><button class="delete"><i class="fas fa-close"></i></button>
      `;
-    const check = value;
-    do {
-        lists.appendChild(list);
-      }
-      while (check!==value);
-      const deleteBtn = list.querySelector('.delete');
-      deleteBtn.addEventListener('click',() => {
-          list.remove();
-          updateLS();   
-      });
+    
+
+    lists.appendChild(list);
+
+  const deleteBtn = list.querySelector(".delete");
+  deleteBtn.addEventListener("click", () => {
+    list.remove();
+    updateLS();
+  });
+//   if(todo.contain){
+//       val.classList.add(strike);
+//   }
+  const val = list.querySelector(".val");
+  val.addEventListener("click", () => {
+      val.classList.toggle('strike');
+  })
 }
 
-
 function updateLS() {
-    const todosEl = document.querySelectorAll("span");
+  const todosEl = document.querySelectorAll("span");
 
-    const todos = [];
+  const todos = [];
 
-    todosEl.forEach((todoEl) => {
-        todos.push(todoEl.innerText);
-    });
-    localStorage.setItem("todos", JSON.stringify(todos));
+  todosEl.forEach((todoEl) => {
+    todos.push(todoEl.innerText,);
+  });
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
