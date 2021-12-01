48265e74baea3717a157292714417c27802b50f8
Ghulam Yazdani <ghulamyazdani12@gmail.com>
2021-12-01 15:11:50 +0530

    search added

--- 10projin24hrs/Movie app/script.js
+++ 10projin24hrs/Movie app/script.js
@@ -5,30 +5,38 @@ const SEARCHAPI =
   "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
   const main = document.getElementById("main");
   const form = document.getElementById("form");
-  
+  const search = document.getElementById("search");
+
 getMovies();
 
 async function getMovies() {
   const resp = await fetch(APIURL);
   const respData = await resp.json();
-  console.log(respData.results);
-  respData.results.forEach((movie) => {
-
+  showMovies(respData.results);
+}
+async function searchedMovies(value){
+  const resp = await fetch(SEARCHAPI+value);
+  const respData = await resp.json();
+  
+  showMovies(respData.results);
+}
+function showMovies(Data) {
+  main.innerHTML = "";
+  Data.forEach((movie) => {
     const movieEl = document.createElement("div");
   
     const { poster_path, title, vote_average } = movie;
     movieEl.classList.add("movie");
     movieEl.innerHTML = `
-            <img src="${IMGPATH + poster_path}" alt="">
+            <img src="${poster_path?IMGPATH + poster_path:IMGPATH+"ckL8dcFg2UAvLGqtNReWdobn6yd.jpg"}" alt="">
             <div class="movie-info">
                 <h3>${title}</h3>
                 <span class="${getClass(vote_average)}">${vote_average}</span>
             </div>`;
     main.appendChild(movieEl);
   });
-  console.log(respData.results);
-  return respData.results;
 }
+
 function getClass(votes) {
   if (votes >= 8) {
     return "green";
@@ -39,3 +47,12 @@ function getClass(votes) {
   }
 }
 
+
+form.addEventListener("submit", (e) =>{
+e.preventDefault();
+const searchTerm = search.value;
+if(searchTerm){
+  searchedMovies(searchTerm);
+}
+
+})
